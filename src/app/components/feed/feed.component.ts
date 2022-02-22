84f54cc61ac884f2c8f2be4e0f77f3f4b9ae8a7b
alaeddin hamroun <alaeddinehamroun@insat.u-carthage.tn>
2022-02-22 23:28:30 +0100

    fixed feed component

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { combineLatest, concat, forkJoin, merge, Observable, Subscription, zip } from 'rxjs';
 import { flatMap, map, mergeMap, zipAll } from 'rxjs/operators';
@@ -21,6 +21,8 @@ export class FeedComponent implements OnInit {
   user: any;
   fromto: string;
   _fromto: string;
+  @ViewChild('scrollBottom') private scrollBottom: ElementRef;
+
   constructor(private chatService: ChatService, private authService: AuthService, public router: Router) {
 
   }
@@ -28,14 +30,24 @@ export class FeedComponent implements OnInit {
 
   ngOnInit(): void {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.scrollToBottom();
 
     this.feedEventSubscription = this.authService.authUser.pipe(mergeMap((user) =>
     combineLatest([this.chatService.getMessages(user.displayName, this.selectedUser.username).valueChanges(), this.chatService.getMessages(this.selectedUser.username, user.displayName).valueChanges()]).pipe(map(x => x[0].concat(x[1])))
   )).subscribe(messages => {
     this.feed = messages
+    this.feed.sort((a,b) => (Date.parse(a.timeSent)>Date.parse(b.timeSent)) ? 1 : ((Date.parse(b.timeSent)>Date.parse(a.timeSent)) ? -1 :0))
     console.log(this.feed)
   })
   }
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+   }
+  scrollToBottom(): void {
+    try {
+        this.scrollBottom.nativeElement.scrollTop = this.scrollBottom.nativeElement.scrollHeight;
+    } catch(err) { }
+}
 
 
 
